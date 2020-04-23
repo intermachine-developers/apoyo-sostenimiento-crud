2d6b0241945ef99e1fe7ae048581c4263825bfed
julian cordoba <dragon_4136@hotmail.com>
2020-04-22 19:36:09 -0500

    python crud update

--- python/static/js/index.js
+++ python/static/js/index.js
@@ -16,71 +16,7 @@ const dataAprendices = document.getElementById('aprendices')
 
 let id = null
 
-document.addEventListener('DOMContentLoaded', function() {
-  render_data()
-  let elems = document.querySelectorAll('select')
-  M.FormSelect.init(elems, { classes: 'options' })
-})
-
-From.addEventListener('submit', e => {
-  id = OID.value
-  if (id == 0) {
-    fetch('/apoyos', {
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify({
-        fullname: fullname.value,
-        email: email.value,
-        phone: Number(phone.value),
-        type_doc: type_doc.value,
-        num_doc: Number(num_doc.value),
-        ficha: Number(ficha.value),
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(data =>
-        M.toast({
-          html: data.messsage,
-          classes: 'green darken-4 rounded',
-          inDuration: 2000,
-        }),
-      )
-      .catch(err => console.error(err))
-  } else {
-    fetch(`/apoyos/${id}`, {
-      method: 'PUT',
-      credentials: 'include',
-      body: JSON.stringify({
-        fullname: fullname.value,
-        email: email.value,
-        phone: Number(phone.value),
-        type_doc: type_doc.value,
-        num_doc: Number(num_doc.value),
-        ficha: Number(ficha.value),
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(data =>
-        M.toast({
-          html: data.messsage,
-          classes: 'blue darken-1 rounded',
-          inDuration: 2000,
-        }),
-      )
-      .catch(err => console.error(err))
-  }
-
-  e.preventDefault()
-  From.reset()
-  render_data()
-})
-
+// functions
 async function render_data() {
   await fetch('/apoyos')
     .then(res => res.json())
@@ -148,3 +84,72 @@ const edit_data = id => {
       ficha.value = data.ficha
     })
 }
+
+//events
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('select')
+  M.FormSelect.init(elems, { classes: 'options' })
+})
+
+From.addEventListener('submit', e => {
+  id = OID.value
+  if (id == 0) {
+    fetch('/apoyos', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        fullname: fullname.value,
+        email: email.value,
+        phone: Number(phone.value),
+        type_doc: type_doc.value,
+        num_doc: Number(num_doc.value),
+        ficha: Number(ficha.value),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data =>
+        M.toast({
+          html: data.messsage,
+          classes: 'green darken-4 rounded',
+          inDuration: 2000,
+        }),
+      )
+      .catch(err => console.error(err))
+  } else {
+    fetch(`/apoyos/${id}`, {
+      method: 'PUT',
+      credentials: 'include',
+      body: JSON.stringify({
+        fullname: fullname.value,
+        email: email.value,
+        phone: Number(phone.value),
+        type_doc: type_doc.value,
+        num_doc: Number(num_doc.value),
+        ficha: Number(ficha.value),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data =>
+        M.toast({
+          html: data.messsage,
+          classes: 'blue darken-1 rounded',
+          inDuration: 2000,
+        }),
+      )
+      .catch(err => console.error(err))
+  }
+
+  e.preventDefault()
+  From.reset()
+  render_data()
+})
+
+
+//start render data
+render_data()
